92b8733a85ddadf7c26fe4096ac9d5328dcd4261
musabhashmi <mushab.hashmi@act21.io>
2026-03-20 12:32:05 +0530

    Vercel endpoint fix

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import API_BASE_URL from "../config/api";
 
 const apiClient = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "/api", // ✅ IMPORTANT (use Vercel rewrite)
   headers: {
     "Content-Type": "application/json",
   },
